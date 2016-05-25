dc443469e4179a52de0e82abad9ed9b00ab4c52c
golden-bear-12 <m.honrychs@gmail.com>
2016-05-25 17:40:36 -0400

    Added User Sign_up, Sign_in, Sign_out

--- Gemfile
+++ Gemfile
@@ -35,3 +35,5 @@ gem 'bootstrap-sass'
 gem 'bcrypt'
 
 gem 'figaro', '1.0'
+
+gem 'devise'
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
+
+  #resources :users, only: [:show]
+
   root 'welcome#home'
 
   # The priority is based upon order of creation: first created -> highest priority.
